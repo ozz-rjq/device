041e9c4bf6ce6efa435e746076d88a8936413c4e
ttm <taras.tataryn.ttm@gmail.com>
2018-01-09 19:22:04 +0200

    Gulpfile reorganization

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
-var gulp = require("gulp");
-var less = require("gulp-less");
-var watch = require("gulp-watch");
+var gulp = require("gulp"),
+		less = require("gulp-less"),
+		browserSync = require("browser-sync");
 
 gulp.task("less", function(){
 	gulp.src("less/style.less")
 		.pipe(less())
-		.pipe(gulp.dest("css"));
+		.pipe(gulp.dest("css"))
+		.pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(['less/*.less', 'less/blocks/*.less', 'less/global/*.less'], ['less']);
+gulp.task("browser-sync", function() {
+	browserSync({
+		server: {
+			baseDir: "./"
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'less'], function() {
+    gulp.watch('less/**/*.less', ['less']);
+    gulp.watch('./*.html', browserSync.reload);
+		gulp.watch('./js/**/*.js', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
