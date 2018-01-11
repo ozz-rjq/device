5288be88b5e1a0265e1dcbaa6b6ca440bcfe3b7e
ttm <taras.tataryn.ttm@gmail.com>
2018-01-11 12:17:37 +0200

    Images optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require("gulp"),
 		less = require("gulp-less"),
-		browserSync = require("browser-sync");
+		browserSync = require("browser-sync"),
+		autoprefixer = require('gulp-autoprefixer');;
 
 gulp.task("less", function(){
 	gulp.src("less/style.less")
 		.pipe(less())
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
 		.pipe(gulp.dest("css"))
 		.pipe(browserSync.reload({stream: true}));
 });
